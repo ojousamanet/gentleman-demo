657c54c650cc48707aadb5747ddd5192399b07b8
黒子猫 <operation@ojousama.net>
2022-03-29 00:46:34 +0900

    コンパイル設定を元に戻す

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
-// import { createApp } from 'vue'
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-// createApp(App).use(router).mount('#app')
-
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).use(router).mount('#app')
